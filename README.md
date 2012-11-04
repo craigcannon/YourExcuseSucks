9365420998836a13ebb7fa6cc1fc137bba4b78f7
Greg Leuch <contact@gleuch.com>
2012-11-04 00:19:45 -0400

    adding scrollto. adding special redirect share page for og:tags to be nicely checked by facebook & nicer stats breakdowns on excuses, pops a redirect back.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Your Excuse Sucks
 
 More information soon.
+
+
+## Smooth Scrolling
+
+Done by data attribute that matches to the index. Doesn't scroll if index does not exist.
--- js/site.js
+++ js/site.js
@@ -8,5 +8,15 @@
 
 */
 
+jQuery.noConflict();
 
-var YourExcuse = {};
+jQuery(document).ready(function() {
+  jQuery('#excuses_list li a').click(function() {
+    var n = jQuery(this).attr('data-excuse'), to = jQuery('#content .excuse a.anchor[data-excuse='+ n +']:eq(0)');
+    if (to.size()) {
+      window.history.replaceState({}, document.title, jQuery(this).attr('href'));
+      to.ScrollTo({duration:500});
+      return false;
+    }
+  });
+});
