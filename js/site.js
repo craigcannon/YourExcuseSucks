855401866231661e98c32fd196e8683c280a9431
Greg Leuch <contact@gleuch.com>
2012-11-05 20:55:23 -0500

    simplifying number system, fixing scrollto (wtf ff?)

--- js/site.js
+++ js/site.js
@@ -12,10 +12,11 @@ jQuery.noConflict();
 
 jQuery(document).ready(function() {
   jQuery('#excuses_list li a').click(function() {
-    var n = jQuery(this).attr('data-excuse'), to = jQuery('#content .excuse a.anchor[data-excuse='+ n +']:eq(0)');
+    var n = jQuery(this).attr('href'), to = jQuery('#content .excuse a.anchor[name="!'+ n +'"]:eq(0)');
+
     if (to.size()) {
-      window.history.replaceState({}, document.title, jQuery(this).attr('href'));
-      to.ScrollTo({duration:500});
+      window.history.replaceState({}, document.title, n);
+      jQuery('html, body').animate({scrollTop: to.offset().top}, 500);
       return false;
     }
   });
